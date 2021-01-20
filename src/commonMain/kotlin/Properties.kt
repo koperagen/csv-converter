a3e5707bf52814817cb04f41ce1aefc5ecde3470
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-01-21 02:14:28 +0300

    Adjust interface purposes

--- src/commonMain/kotlin/Properties.kt
+++ src/commonMain/kotlin/Properties.kt
@@ -1,6 +1,6 @@
 data class Row(val id: TextId, val text: String)
 
-data class PropertiesFile(val file: String, val contents: List<Row>)
+data class PropertiesFile(val groupName: String, val locale: Locale, val contents: List<Row>)
 
 fun Iterable<String>.parse(): Iterable<Row> =
     filterNot { it.startsWith('#') || it.isBlank() }
@@ -11,13 +11,24 @@ fun Iterable<String>.parse(): Iterable<Row> =
         )
     }
 
-interface LocalizationContext {
+interface TranslationService {
     suspend fun translate(source: String, translation: Translation): String
 
     data class Translation(val from: Locale, val to: Locale)
 }
 
+interface LocalizationContext {
+    suspend fun translate(row: Row, translation: TranslationService.Translation): Row
+}
+
 suspend fun LocalizationContext.localize(
     file: PropertiesFile,
     locales: List<Locale>
-): Localization = TODO()
+): List<PropertiesFile> {
+    return locales.map {
+        val localizedContent = file.contents.map { row ->
+            translate(row, TranslationService.Translation(file.locale, it))
+        }
+        file.copy(locale = it, contents = localizedContent)
+    }
+}
