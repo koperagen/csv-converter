737f6b74fa6a1cd632f8683657ebd3919dfc26a2
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-01-21 11:43:57 +0300

    Ensure localization creates with all locales

--- src/commonMain/kotlin/Localization.kt
+++ src/commonMain/kotlin/Localization.kt
@@ -1,6 +1,6 @@
 import ParseResult.*
 
-data class Localization(val file: String, val texts: List<Text>)
+data class Localization(val file: String, val locales: Set<Locale>, val texts: List<Text>)
 
 data class Text(val id: TextId, val variants: Map<Locale, PreEditedText>)
 
@@ -18,21 +18,27 @@ typealias Locale = String
  */
 @OptIn(ExperimentalStdlibApi::class)
 fun parse(rawData: List<Map<String, String>>): ParseResult {
+    val anyRow = rawData.firstOrNull() ?: return Error.EmptyFile
+    val locales: Set<Locale> = anyRow.keys - COL_ID
+
     val data = buildList {
         for (row in rawData) {
             val id = row[COL_ID] ?: return Error.MissingIdError(row)
-            val variants = (row - COL_ID).mapValues { (_, value) -> PreEditedText.of(value) }
-            add(Text(id, variants))
+            val variants = (row - COL_ID)
+            if (variants.keys != locales) return Error.DifferentKeySet(locales, variants)
+            add(Text(id, variants.mapValues { (_, value) -> PreEditedText.of(value) }))
         }
     }
-    return Data(data)
+    return Data(locales, data)
 }
 
 sealed class ParseResult {
-    data class Data(val data: List<Text>) : ParseResult()
+    data class Data(val locales: Set<Locale>, val data: List<Text>) : ParseResult()
 
     sealed class Error : ParseResult() {
         data class MissingIdError(val row: Map<String, String>) : Error()
+        object EmptyFile : Error()
+        data class DifferentKeySet(val expectedKeys: Set<Locale> , val row: Map<String, String>) : Error()
     }
 }
 
