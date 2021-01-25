61b5a1cc4765dcd4c31fb7ea080cc8ff02475e17
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2021-01-25 08:23:51 +0300

    Add executable for CSV initialization

---
+++ src/jvmMain/kotlin/csv/InitCsv.kt
@@ -0,0 +1,30 @@
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
+import java.io.File
+
+private const val PATH = "/home/nikita/AndroidStudioProjects/hackersimulator/android/src/main/assets/lang/basic_lang.properties"
+private const val DEST = "/home/nikita/IdeaProjects/csv-converter/output"
+
+private const val INPUT_DIRECTORY = "/home/nikita/AndroidStudioProjects/hackersimulator/android/src/main/assets/lang/"
+
+fun main() {
+    val files = File(INPUT_DIRECTORY).walkTopDown().filter { it.extension == "properties" }
+    for (file in files) {
+        initCsv(file, "ru", File(DEST), setOf("en"))
+    }
+}
+
+internal fun initCsv(input: File, inputLocale: Locale, outputDirectory: File, locales: Set<Locale>) {
+    val properties = propertiesFile(input, inputLocale)
+    val csv = init(properties, locales)
+    File(outputDirectory, csv.filename).outputStream().use {
+        csvWriter().write(csv, it)
+    }
+}
+
+internal fun propertiesFile(file: File, locale: Locale): PropertiesFile {
+    val group = file.nameWithoutExtension.let {
+        it.substringBeforeLast("_", it)
+    }
+    val contents = file.useLines { it.asIterable().parse() }
+    return PropertiesFile(group, locale, contents)
+}
